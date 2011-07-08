8ce69e79455c08f7aaf662db638c6b573a255bd7
Rohit <mprohit@gmail.com>
2011-07-08 18:36:31 -0400

    added lengthsquared methods

--- src/org/mozilla/javascript/classy/TestInlineCaching.java
+++ src/org/mozilla/javascript/classy/TestInlineCaching.java
@@ -9,52 +9,62 @@ import org.junit.Test;
 public class TestInlineCaching extends ClassyScriptable{
 	
 	
-	static TestInlineCaching t0 = null,t1=null, t2=null;
+	static TestInlineCaching t0 = null,t1=null, t2=null, test=null;
 	static Map<String,Integer> hm;
-	ClassyLayout possibleType = null;
-	int possibleOffset;
+	static ClassyLayout possibleType = null;
+	static int possibleOffset;
 	
 	static{
 
 		t0 = new TestInlineCaching();
-		t0.put("x", null, 100);
-		t0.put("y", null, 200);
-		t0.put("z", null, 300);
+		t0.put("x", null, 1);
+		t0.put("y", null, 2);
+		t0.put("z", null, 3);
+		
+		test = new TestInlineCaching();
+		test.put("x", null, 4);
+		test.put("y", null, 5);
+		test.put("z", null, 6);
 		
 		t1 = new TestInlineCaching();
 		t1.put("name", null, "rohit");
-		t1.put("x",null,10);
+		t1.put("x", null, 10);
 		t1.put("y", null, 20);
 		
 		t2 = new TestInlineCaching();
 		t2.put("name", null, "hannes");
-		t2.put("x", null, 15);
-		t2.put("y", null, 25);
+		t2.put("x", null, 100);
+		t2.put("y", null, 200);
 		
 		hm = new HashMap<String, Integer>();
 		hm.put("x",10);
-		hm.put("p",20);
+		hm.put("y",20);
 		
 	}
 	
 
 	public static void main(String[] args) {
-
-		int value = (Integer) t0.getWithInlineCaching("x");
-		int value1 = (Integer)t0.get("x",null);
-
-		System.out.println(value);
-		System.out.println(value1);
 		
-		System.out.println(t1.getWithInlineCaching("y"));
-		System.out.println(t1.getWithInlineCaching("x"));
+		System.out.println(lengthSquared(t0));
+		System.out.println(lengthSquared(test));
+		System.out.println(lengthSquared(t1));
+		System.out.println(lengthSquared(t2));
+
+		System.out.println(lengthSquaredWithInlineCaching(t0));
+		lengthSquaredWithInlineCaching(t0);
+		System.out.println(lengthSquaredWithInlineCaching(test));
+		System.out.println(lengthSquaredWithInlineCaching(t1));
+		System.out.println(lengthSquaredWithInlineCaching(t2));
+		
+		System.out.println(lengthSquaredWithHashMap(hm));
+
 
 		long time = 0;
 
 		for(int  i=0;i<10;i++){
 			time = System.currentTimeMillis();
 			for(int j=0;j<10000000;j++){
-				t1.get("x");
+				lengthSquared(t0);
 			}
 			System.out.print(System.currentTimeMillis() - time+"\t");
 		}
@@ -64,22 +74,21 @@ public class TestInlineCaching extends ClassyScriptable{
 		for(int  i=0;i<10;i++){
 			time = System.currentTimeMillis();
 			for(int j=0;j<10000000;j++){
-				t1.getWithInlineCaching("x");
+				lengthSquaredWithInlineCaching(t0);
 			}
 			System.out.print(System.currentTimeMillis() - time+"\t");
 		}
 		
+
 		System.out.println();
 		
 		for(int  i=0;i<10;i++){
 			time = System.currentTimeMillis();
 			for(int j=0;j<10000000;j++){
-				hm.get("x");
+				lengthSquaredWithHashMap(hm);
 			}
 			System.out.print(System.currentTimeMillis() - time+"\t");
 		}
-		
-		lengthSquared(t0);
 		
 		
 		
@@ -88,18 +97,18 @@ public class TestInlineCaching extends ClassyScriptable{
 
 	private Object getWithInlineCaching(String key) {
 
-		if(this.possibleType == null){
-			Map<String,Object> typeInformatioMap = extendedGet(key, null);
-			this.possibleType = (ClassyLayout)typeInformatioMap.get("type");
-			this.possibleOffset = (Integer)typeInformatioMap.get("offset");
+		if(possibleType == null){
+			Map<String,Object> typeInformatioMap = this.extendedGet(key, null);
+			possibleType = (ClassyLayout)typeInformatioMap.get("type");
+			possibleOffset = (Integer)typeInformatioMap.get("offset");
 			return typeInformatioMap.get("value");
 		}else{
 			ClassyLayout cuurentType = this.getTypeOfObject();
-			if(cuurentType == this.possibleType){
-				this.possibleOffset = this.getoffset(key);
-				return this.getValueAtOffset(this.possibleOffset);
+			if(cuurentType == possibleType){
+				possibleOffset = this.getoffset(key);
+				return this.getValueAtOffset(possibleOffset);
 			}else{
-				this.possibleType = null;
+				possibleType = null;
 				return getWithInlineCaching(key);
 			}
 		}
@@ -112,6 +121,10 @@ public class TestInlineCaching extends ClassyScriptable{
 	
 	private static int lengthSquaredWithInlineCaching(TestInlineCaching tic){
 		return (Integer)tic.getWithInlineCaching("x") * (Integer) tic.getWithInlineCaching("y");
+	}
+	
+	private static int lengthSquaredWithHashMap(Map<String,Integer> hmap){
+		return (Integer)hmap.get("x") * (Integer)hmap.get("y");
 	}
 
 
